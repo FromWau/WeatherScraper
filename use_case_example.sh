d80f55cdfc4fe7271c83fa0cc2a08859cd37f16c
FromWau <tobias.frommhund@aon.at>
2022-10-22 08:52:00 +0200

    Updated use_case_example.sh

--- use_case_example.sh
+++ use_case_example.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# To change city change the LOCATION_CODE.
+LOCATION_CODE='38d35340a098212bc3ae7aa6ff89ce95a7eed769997999f8ad0804d2f3ccd560'
+# ==============================================================================
+
+
 info_to_icon() {    
     case $1 in
         'Sunny')
@@ -27,21 +32,25 @@ info_to_icon() {
         esac
 }
 
-json=$(python weather_scraper.py 38d35340a098212bc3ae7aa6ff89ce95a7eed769997999f8ad0804d2f3ccd560)
+
+# weather_scraper.py needs to be in same dir as this script.
+json=$(python weather_scraper.py "$LOCATION_CODE")
 weather=$(echo "$json" | jq '.location.forecasts' | jq '.[0].weather')
 
 
 # forecast 1
 tmp1=$(echo "$weather" | jq -r '.[0].temperature')
-typ1=$(echo "$weather" | jq -r '.[0].info')
 tmp1_num=$(echo "$tmp1" | tr -d '°')
-icon1=$(info_to_icon "$typ1")
+
+info1=$(echo "$weather" | jq -r '.[0].info')
+icon1=$(info_to_icon "$info1")
 
 # forecast 2
 tmp2=$(echo "$weather" | jq -r '.[1].temperature')
-typ2=$(echo "$weather" | jq -r '.[1].info')
 tmp2_num=$(echo "$tmp2" | tr -d '°')
-icon2=$(info_to_icon "$typ2")
+
+info2=$(echo "$weather" | jq -r '.[1].info')
+icon2=$(info_to_icon "$info2")
 
 # Set the arrow for the corresponding diff temps
 if [ "$tmp1_num" -lt "$tmp2_num" ]; 
@@ -54,4 +63,6 @@ else
     tmp_diff='↘ '
 fi
 
+# output for polybar
 echo "$icon1 $tmp1 $tmp_diff $icon2 $tmp2"
+
